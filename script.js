be1691b9056d5752a6a1d4cb7acadae065c74007
noeamelya <noeamelyaofficial@hotmail.com>
2023-08-04 20:17:22 +0100

    Add function timeblocks for standard business hours

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-//Time
-var update = dayjs("Tuesday, August 08, 2023").format("dddd, MMMM D, YYYY");
-$(".currentDay").text(update);
-console.log(update);
+
+// Display the current day at the top of the calender when a user opens the planner.
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
+console.log(todayDate);
+
+
+// Present timeblocks for standard business hours when the user scrolls down.
+
+
+// Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+
+// Allow a user to enter an event when they click a timeblock
+
+// Save the event in local storage when the save button is clicked in that timeblock.
+
+// Persist events between refreshes of a page
